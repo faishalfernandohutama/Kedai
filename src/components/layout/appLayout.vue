66c5a591d57d2184e3ec6e1388d112bcf4918418
Faishal Fernando Hutama <faishalfernandohutama@192.168.1.3>
2026-04-09 18:49:55 +0700

    Penyempurnaan layout

--- src/components/layout/appLayout.vue
+++ src/components/layout/appLayout.vue
@@ -1,29 +1,42 @@
 <script setup lang="ts">
-import { Icon } from '@iconify/vue';
-
+  import { Icon } from "@iconify/vue";
 </script>
 
 <template>
-    <section>
-        <div class="flex flex-col-reverse md:flex-row h-screen bg-gray-50 overflow-hidden">
-            <aside class="w-full h-16 bg-white border-t border-gray-200 flex items-center justify-around z-50 shrink-0
-            md:w-24 md:h-screen md:flex-col md:justify-start md:border-t-0 md:border-r md:py8">
-            <div class="hidden md:flex mb-12">
-                  <Icon icon="lucide:flower-2" class="w-8 h-8 text-orange-600" />
-            </div>
-                <div>
-                    <Icon icon="lucide:layout-grid" class="w-6 h-6" />
-                </div>
-                <div>
-                    <Icon icon="lucide:layout-grid" class="w-6 h-6" />
-                </div>
-                <div>
-                    <Icon icon="lucide:layout-grid" class="w-6 h-6" />
-                </div>
-                <div>
-                    <Icon icon="lucide:layout-grid" class="w-6 h-6" />
-                </div>
-            </aside>
+  <div
+    class="flex flex-col-reverse md:flex-row h-screen bg-gray-50 overflow-hidden"
+  >
+    <aside
+      class="w-full h-16 bg-white border-t border-gray-200 flex items-center justify-around z-50 shrink-0 md:w-24 md:h-screen md:flex-col md:justify-start md:border-t-0 md:border-r md:py-8"
+    >
+      <div class="hidden md:flex mb-12">
+        <Icon icon="lucide:flower-2" class="w-8 h-8 text-mm" />
+      </div>
+      <!-- menu -->
+      <div
+        class="flex md:flex-col gap-8 md:gap-6 items-center w-full md:w-auto justify-evenly"
+      >
+        <div class="p-3 cursor-pointer text-gray-500 hover:text-gray-500 hover:bg-gray-100 rounded-2xl transition-colors">
+          <Icon icon="lucide:layout-grid" class="w-6 h-6" />
         </div>
-    </section>
+        <div class="p-3 cursor-pointer text-gray-500 hover:text-gray-500 hover:bg-gray-100 rounded-2xl transition-colors">
+          <Icon icon="lucide:layout-grid" class="w-6 h-6" />
+        </div>
+        <div class="p-3 cursor-pointer text-gray-500 hover:text-gray-500 hover:bg-gray-100 rounded-2xl transition-colors">
+          <Icon icon="lucide:layout-grid" class="w-6 h-6" />
+        </div>
+        <div class="p-3 cursor-pointer text-gray-500 hover:text-gray-500 hover:bg-gray-100 rounded-2xl transition-colors">
+          <Icon icon="lucide:layout-grid" class="w-6 h-6" />
+        </div>
+      </div>
+
+      <div class="hidden md:flex mt-auto mb-4 p-3 cursor-pointer text-gray-500 hover:text-gray-800 hover:bg-gray-100 rounded-2xl transition-colors">
+        <Icon icon="lucide:layout-grid" class="w-6 h-6" />
+      </div>
+    </aside>
+
+    <main class="flex-1 overflow-y-auto">
+      <slot />
+    </main>
+  </div>
 </template>
--- src/components/mainPage.vue
+++ src/components/mainPage.vue
@@ -1,45 +1,46 @@
 <script setup>
-import { Icon } from "@iconify/vue";
-import AppLayout from "./layout/appLayout.vue";
+  import { Icon } from "@iconify/vue";
+  import AppLayout from "./layout/appLayout.vue";
 </script>
 
 <template>
-<AppLayout/>
-  <section class="min-h-screen px-10 py-10 bg-gray-50">
-    <div>
-      <nav class="flex justify-between items-center">
-        
-        <div class="flex w-full max-w-xl">
-          <div
-            class="flex items-center w-full bg-white border border-gray-300 py-2.5 px-5 rounded-full focus-within:border-blue-500 focus-within:ring-2 focus-within:ring-blue-100 transition-all"
-          >
-            <Icon icon="lucide:search" class="text-gray-400 w-5 h-5 mr-3" />
-            <input
-              type="text"
-              placeholder="Search menu"
-              class="w-full bg-transparent border-none outline-none text-gray-700 placeholder-gray-400 text-sm"
-            />
-          </div>
-        </div>
-
-        <div class="flex items-center gap-4 ml-8 cursor-pointer">
-          
-          <div class="flex flex-col text-right hidden sm:flex">
-            <span class="text-sm font-semibold text-gray-800">Faishal Fernando</span>
-            <span class="text-xs text-gray-500">Web Developer</span>
+  <AppLayout>
+    <section class="min-h-screen px-10 py-10 bg-gray-50">
+      <div>
+        <nav class="flex justify-between items-center">
+          <div class="flex w-full max-w-xl">
+            <div
+              class="flex items-center w-full bg-white border border-gray-300 py-2.5 px-5 rounded-full focus-within:border-blue-500 focus-within:ring-2 focus-within:ring-blue-100 transition-all"
+            >
+              <Icon icon="lucide:search" class="text-gray-400 w-5 h-5 mr-3" />
+              <input
+                type="text"
+                placeholder="Search menu"
+                class="w-full bg-transparent border-none outline-none text-gray-700 placeholder-gray-400 text-sm"
+              />
+            </div>
           </div>
 
-          <div class="w-10 h-10 rounded-full border-2 border-gray-200 overflow-hidden bg-white flex-shrink-0">
-            <img 
-              src="https://api.dicebear.com/7.x/notionists/svg?seed=Faishal" 
-              alt="Profile" 
-              class="w-full h-full object-cover" 
-            />
-          </div>
-          
-        </div>
+          <div class="flex items-center gap-4 ml-8 cursor-pointer">
+            <div class="flex flex-col text-right hidden sm:flex">
+              <span class="text-sm font-semibold text-gray-800"
+                >Faishal Fernando</span
+              >
+              <span class="text-xs text-gray-500">Web Developer</span>
+            </div>
 
-      </nav>
-    </div>
-  </section>
+            <div
+              class="w-10 h-10 rounded-full border-2 border-gray-200 overflow-hidden bg-white flex-shrink-0"
+            >
+              <img
+                src="https://api.dicebear.com/7.x/notionists/svg?seed=Faishal"
+                alt="Profile"
+                class="w-full h-full object-cover"
+              />
+            </div>
+          </div>
+        </nav>
+      </div>
+    </section>
+  </AppLayout>
 </template>
